9a32430fc66783acf26706a5a9d6a6837ac47a19
Moaney <ladonnamoney@gmail.com>
2019-08-26 18:00:51 -0400

    added an onClick function on the number buttons, they now add a number to the display

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -5,7 +5,8 @@ import NumberButton from './NumberButton';
 //Import your array data to from the provided data file
 
 import { numbers } from '../../../data';
-const Numbers = () => {
+const Numbers = (props) => {
+  console.log("Numbers", props)
   // STEP 2 - add the imported data to state
   const [buttonNumbers, setButtonNumbers] = useState(numbers);
   return (
@@ -13,7 +14,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {buttonNumbers.map(number => <NumberButton key={number} text={number} />)}
+       {buttonNumbers.map(number => <NumberButton key={number} text={number} addNumber={props.addNumber} />)}
     </div>
   );
 };
